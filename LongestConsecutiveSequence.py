f5144ef92956794f2fcc48757edb629ce8a9331e
ERichard007 <ecrb29@umsystem.edu>
2025-04-13 17:45:36 -0500

    started

---
+++ LongestConsecutiveSequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestConsecutive(self, nums: list[int]) -> int:
+        
+
+
+        return 1
+   
+
+
+nums = [2,20,4,10,3,4,5]
+mySolution = Solution()
+print(mySolution.longestConsecutive(nums))
