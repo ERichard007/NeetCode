6448c91fd3fa75379654e8c28eadcbcb68d6dd32
ERichard007 <ecrb29@umsystem.edu>
2025-04-13 23:52:50 -0500

    longestConsecutiveSequence

--- LongestConsecutiveSequence.py
+++ LongestConsecutiveSequence.py
@@ -1,29 +1,17 @@
 class Solution:
     def longestConsecutive(self, nums: list[int]) -> int:
         mySet = set(nums)
-        print(mySet)
 
         longestSequence = 0
-        ct = 0
-        lastNum = None
-        for num in mySet:
-            if lastNum == None: 
-                lastNum = num
-                ct += 1 
-                longestSequence = max(ct, longestSequence)
-                continue
-
-            if num == lastNum+1:
-                ct += 1
-                lastNum = num
-            else:
-                ct = 0
-            
-            longestSequence = max(ct, longestSequence)
+        for num in nums:
+            length = 0
+            while (num + length) in mySet:
+                length+=1
+            longestSequence = max(longestSequence, length)
 
         return longestSequence
 
-nums = [0,-1]
+nums = [2,20,4,10,3,4,5]
 
 mySolution = Solution()
 print(mySolution.longestConsecutive(nums))
