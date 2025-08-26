2e22cb9c172edfc70b804d83f86b67055c9cccb5
ERichard007 <ecrb29@umsystem.edu>
2025-08-26 15:11:15 -0500

    binary search for some reason always messed with me dont know why its done though

--- Binary Search/BinarySearch.py
+++ Binary Search/BinarySearch.py
@@ -2,30 +2,35 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        high = len(nums)-1 #3
-        low = 0 #2
-        mid = int(len(nums)/2) #2
-
         if len(nums) == 1:
-            if target == nums[0]:
+            if (nums[0] == target):
                 return 0
             else:
                 return -1
 
-        while (mid < high and mid > low):
-            if (target > nums[mid]):
-                low = mid
-                mid += int((high-low)/2)
-            elif (target < nums[mid]):
-                high = mid
-                mid -= int((high-low)/2)
+
+        high = len(nums)-1 #3
+        low = 0 #2
+        mid = low + int((high-low)/2)
+
+        while (low <= high):
+            print("high: ", nums[high], " low: ", nums[low], " mid: ", nums[mid])
+
+            if (nums[mid] < target):
+                low = mid+1
+                mid = low + int((high-low)/2)
+            elif (nums[mid] > target):
+                high = mid-1
+                mid = low + int((high-low)/2)
             elif (target == nums[mid]):
                 return mid
         
+        print("high: ", nums[high], " low: ", nums[low], " mid: ", nums[mid])
         return -1
 
         
 
 mySolution = Solution()
-
-print(mySolution.search([-1,0,2,4,6,8], 3)) #should output 3
+nums=[-1,0,2,4,6,8]
+target=3
+print(mySolution.search(nums, target)) 
