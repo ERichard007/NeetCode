ddbec85e0901a58c81e44f6b92652d7550a6f4b2
ERichard007 <ecrb29@umsystem.edu>
2025-08-30 19:18:44 -0500

    LinkedListCycle

--- Linked List/LinkedListCycle.py
+++ Linked List/LinkedListCycle.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -5,6 +7,30 @@ class ListNode:
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        return True
+        if head.next == None or head.next.next == None: return False
+
+        slowPointer = head
+        fastPointer = head.next.next 
+
+        while fastPointer != None:
+            print("Slow: " + str(slowPointer.val) + " Fast: " + str(fastPointer.val))
+            slowPointer = slowPointer.next
+            fastPointer = fastPointer.next.next if fastPointer.next != None else None
+
+            if slowPointer == fastPointer:
+                print("Slow: " + str(slowPointer.val) + " Fast: " + str(fastPointer.val))
+                return True
+
+        return False
     
 
+mySolution = Solution()
+
+listNode4 = ListNode(4)
+listNode3 = ListNode(3,listNode4)
+listNode2 = ListNode(2,listNode3)
+listNode1 = ListNode(1,listNode2)
+
+listNode4.next = listNode2
+
+print(mySolution.hasCycle(listNode1))
