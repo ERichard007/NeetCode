8096fda376a99e8369990a707dd40583f792e163
ERichard007 <ecrb29@umsystem.edu>
2025-09-02 17:45:54 -0500

    keep tyyring

--- Trees/DiameterOfBinaryTree.py
+++ Trees/DiameterOfBinaryTree.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -7,16 +9,26 @@ class TreeNode:
         self.left = left
 
 class Solution:
+    MAX_D = 0
+
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         if not root:
-            return 0
-        
-        width = self.diameterOfBinaryTree(root.left) + self.diameterOfBinaryTree(root.right) + 1
+            return -1
 
-        return width
+        leftHeight = self.diameterOfBinaryTree(root.left) + 1
+        rightHeight = self.diameterOfBinaryTree(root.right) + 1
+
+        self.MAX_D = max(leftHeight+rightHeight, self.MAX_D)
+
+        print("root value: " + str(root.val) + " left height: " + str(leftHeight) + " right height: " + str(rightHeight) + " max depth: " + str(self.MAX_D))
+
+        return max(leftHeight,rightHeight)
+        
 
 mySolution = Solution()
 
-tree = TreeNode(1,TreeNode(2),TreeNode(3))
+tree = TreeNode(1,None,TreeNode(2,TreeNode(3,TreeNode(5)), TreeNode(4)))
 
-print(mySolution.diameterOfBinaryTree(tree))
+mySolution.diameterOfBinaryTree(tree)
+
+print(mySolution.MAX_D)
